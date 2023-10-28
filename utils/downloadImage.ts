80f2ad63cb335b9f93be79d9a46a887ba2cdacce
Viorel Mocanu PTT <viorel.mocanu@pentest-tools.com>
2023-10-28 16:21:44 +0300

    fix: fix conflicts with main

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -1,21 +1,14 @@
-import { makeImgFolder, removeImgFolder, removeResourceFolder } from './filesManager'
+import axios from 'axios'
 import { createWriteStream } from 'fs'
+import { makeImgFolder } from './filesManager'
 import { markdownDestination } from '../config'
 import path from 'path'
-import request from 'request'
 
-export function downloadImage(url: string, title: string) {
+export async function downloadImage(url: string, title: string) {
 	const filePath = path.join(markdownDestination, title, 'img', `${title}${path.extname(url)}`)
 	makeImgFolder(title)
 
-	request(url)
-		.pipe(createWriteStream(filePath))
-		.on('ready', () => {
-			console.info('Image downloaded successfully!')
-		})
-		.on('error', (err) => {
-			console.error('Error downloading image', err)
-			removeResourceFolder(title)
-			removeImgFolder(title)
-		})
+	const response = await axios.get(url, { responseType: 'stream' })
+
+	await response.data.pipe(createWriteStream(filePath))
 }
