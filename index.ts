a574c26893c8be93d9c3e568c82454f1b5f2d1c9
Petre Gabriel <petregabriel1407@gmail.com>
2023-11-02 18:26:11 +0200

    refactor index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-import { generateResourceFileWithMetadata } from './src/utils/generateResourceFileWithMetadata'
-
-async function main() {
-	await generateResourceFileWithMetadata('https://www.youtube.com/watch?v=3XaXKiXtNjw')
-}
-
-main()
-
-export default generateResourceFileWithMetadata
+export { generateResourceFileWithMetadata } from './src/utils/generateResourceFileWithMetadata'
